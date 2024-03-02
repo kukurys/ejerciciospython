6a6655da2a22c042e7b51c190282b521a2451d16
unknown <SENA@IBADFPCIIFS190.FormacionSena.RED>
2024-03-02 09:21:25 -0500

    actualizacion 1

---
+++ secuenciales/trabajo_1.py
@@ -0,0 +1,11 @@
+#declaracion de variables x y y 
+x=2
+y=3
+#recuperar las dos variables 
+suma=x+y
+resta=x-y
+producto=x*y 
+division=x/y
+modulo=x%y
+print("el resultado de la suma es ",suma)
+print(f"la resta de {x} y {y} es {resta}")
